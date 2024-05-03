81015c077f577c2d523db53ca9e0d0ff84257d59
Siddharth Soni <54449601+siddharthsonii@users.noreply.github.com>
2024-05-03 18:43:47 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -39,7 +39,7 @@ function ResumeNew() {
         <Row className="resume">
           <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess} className="d-flex justify-content-center">
             {Array.from(new Array(Math.ceil(numPages / 2)), (el, index) => (
-              <Row key={`row_${index}`} className="d-flex justify-content-center">
+              <Row key={`row_${index}`}>
                 <Page key={`page_${index * 2 + 1}`} pageNumber={index * 2 + 1} scale={width > 786 ? 1.7 : 0.6} />
                 {index * 2 + 2 <= numPages && (
                   <Page key={`page_${index * 2 + 2}`} pageNumber={index * 2 + 2} scale={width > 786 ? 1.7 : 0.6} />
